bf35ebb21d42ec30ce7a917a90310c42b19d7830
salzoubi <sal.alzoubi123@gmail.com>
2025-01-03 23:10:03 +0300

    commit 8
    (cherry picked from commit 8e6751fd727f7daa659104e1f192fdbcc0312799)

---
+++ src/app/components/general/header/header.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, ViewChild, HostListener, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {trigger, style, query, transition, stagger, animate } from '@angular/animations'
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+import { TranslateService } from '@ngx-translate/core';
+import { UntypedFormControl } from '@angular/forms';
+import { LanguageService } from 'src/app/services/language/language.service';
+import { ThisReceiver } from '@angular/compiler';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  animations:[
+    trigger('animateMenu', [
+      transition(':enter', [
+        query('*', [
+          style({opacity: 0, transform: 'translateY(-50%)'}),
+          stagger(50, [
+            animate(
+              '250ms cubic-bezier(0.35, 0, 0.25, 1)',
+              style({opacity: 1, transform: 'none'}))
+          ])
+        ])
+      ])
+    ])
+  ]
+})
+
+
+
+export class HeaderComponent implements OnInit {
+
+  responsiveMenuVisible: Boolean = false;
+  pageYPosition: number;
+  languageFormControl: UntypedFormControl = new UntypedFormControl();
+
+  constructor(
+    private router: Router,
+    public analyticsService: AnalyticsService,
+    public languageService: LanguageService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.languageFormControl.valueChanges.subscribe(val => this.languageService.changeLanguage(val));
+
+    this.languageFormControl.setValue(this.languageService.language);
+
+  }
+  // tslint:disable-next-line:typedef
+  scroll(el) {
+    if (document.getElementById(el)) {
+      document.getElementById(el).scrollIntoView({behavior: 'smooth'});
+    } else{
+      this.router.navigate(['/home']).then(() => document.getElementById(el).scrollIntoView({behavior: 'smooth'}) );
+    }
+    this.responsiveMenuVisible = false;
+  }
+
+  // tslint:disable-next-line:typedef
+  openCV() {
+    // Replace the URL below with your actual public Google Drive link
+    const cvUrl = 'https://drive.google.com/file/d/1M1y0zL81gOxsH7hUAedRbeGeJG8vDQAB/view?usp=drive_link';
+    window.open(cvUrl, '_blank'); // Opens in a new tab
+  }
+
+  @HostListener('window:scroll', ['getScrollPosition($event)'])
+    getScrollPosition(event) {
+        this.pageYPosition=window.pageYOffset
+    }
+
+    changeLanguage(language: string) {
+      this.languageFormControl.setValue(language);
+    }
+}
